f0d9b23ec2c9cda01f80306af11d0e4c82459e44
gurkankalkancompany <nesimigurkankalkan@gmail.com>
2021-08-17 13:41:47 +0300

    BranchDenemesi2

--- MVCMyWebSite/Controllers/KULLANICILARController.cs
+++ MVCMyWebSite/Controllers/KULLANICILARController.cs
@@ -33,6 +33,7 @@ namespace MVCMyWebSite.Controllers
                     
                     if (User != null)
                     {
+                    //BranchDenemesi
                     //Http Cookie ile Session Aç
                     //JavaScriptSerializer js = new JavaScriptSerializer();
                     //LoginVM usr = new LoginVM
