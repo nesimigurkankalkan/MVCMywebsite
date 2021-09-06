e25ce0b3ee6658d02f08114970777863192abaec
gurkankalkancompany <nesimigurkankalkan@gmail.com>
2021-09-06 17:00:05 +0300

    Added Labs

---
+++ MVCMyWebSite/Controllers/LabsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMyWebSite.Controllers
+{
+    public class LabsController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
--- MVCMyWebSite/Models/MysiteModel.Designer.cs
+++ MVCMyWebSite/Models/MysiteModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\NK\Documents\Visual Studio 2015\Projects\MVCMyWebSiteWithoutAjaxWithViewModel\MVCMyWebSite\Models\MysiteModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\gurkan.kalkan\Desktop\GurkanProjects\github\CloneProjects\MVCMywebsite\MVCMyWebSite\Models\MysiteModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
